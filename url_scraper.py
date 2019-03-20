9c10a939be87a23856b7364815a77f08702df48b
Kirk Bozeman <kirkbozeman@gmail.com>
2019-03-20 18:42:33 -0500

    multiprocessing for url scraper for future use

--- url_scraper.py
+++ url_scraper.py
@@ -2,50 +2,91 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup, SoupStrainer
 from time import sleep
 from mysql_con import mysql_con
+from multiprocessing import Pool, cpu_count
 
 
-def get_links():
+def get_links_history():
+    """
+    Function gets current full link history
+
+    """
+    cn = mysql_con("Dev")
+    with cn.cursor() as cursor:
+        sql = "SELECT DISTINCT url FROM album_urls"
+        cursor.execute(sql)
+
+    return [item["url"] for item in cursor.fetchall()]
+
+
+def get_links(url):
     """
     Function used to retrieve available album review links from pitchfork.com/reviews/albums/...
-    Used to scrape links thru 2/28/19 (any site design changes may require code adjustments or rebuild)
 
     """
 
-    for ix in range(1, 2000):  # force retry certain # times? # 1754 was last page on 2.28.19
+    url_hist = get_links_history()
 
-        print(ix)  # debug print page num
-        url = "https://pitchfork.com/reviews/albums/?page=" + str(ix)
+    try:
 
-        driver = webdriver.Safari()
+        #driver = webdriver.Safari()  # multi will not work with safari
+        options = webdriver.ChromeOptions()
+        #options.add_argument("headless")
+        driver = webdriver.Chrome(options=options)
         driver.get(url)
 
-        try:
-            element = WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "review")))  # review link data is in class=review
-            sleep(3)  # wait additional (to be safe)
+        element = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "review")))  # review link data is in class=review
+        sleep(3)  # wait additional (to be safe)
 
-            soup = BeautifulSoup(driver.page_source, features="lxml", parse_only=SoupStrainer('a'))
+        soup = BeautifulSoup(driver.page_source, features="html.parser", parse_only=SoupStrainer('a'))
+#        soup = BeautifulSoup(driver.page_source, features="lxml", parse_only=SoupStrainer('a'))
 
-            cn = mysql_con("Dev")
+        cn = mysql_con("Dev")
 
-            for link in soup:
+        for link in soup.findAll('a'):
 
-                if link.has_attr('href') \
-                        and str(link).find("/reviews/albums/") > 0 > str(link).find("?") \
-                        and str(link['href']) != "/reviews/albums/":
+             if str(link).find("/reviews/albums/") > 0 > str(link).find("?") \
+                    and str(link['href']) != "/reviews/albums/":
 
+                href = str(link['href'])
+                print(href)
+
+                if href in url_hist:
+                    print(f"Link {href} already exists.")
+                else:
                     with cn.cursor() as cursor:
                         sql = "INSERT INTO album_urls (url) VALUES (%s)"
-                        cursor.execute(sql, (str(link['href'])))
-                        cn.commit()
+                        cursor.execute(sql, href)
+                    cn.commit()  # must be o/s with
 
-        finally:
-            driver.quit()
+    except Exception as e:
+        print(e)
+        pass  # scraping should continue on fail
+
+    finally:
+        driver.quit()
+
+    return 0
 
 
 if __name__ == '__main__':
-    get_links()
 
+    urls = ["https://pitchfork.com/reviews/albums/?page=" + str(ix) for ix in range(1,10)]
+
+    try:
+
+        # run multiprocessing
+        pool = Pool(2)
+        results = [pool.apply_async(get_links, (url,)) for url in urls]
+        print([r.get() for r in results])
+
+    except Exception as e:
+        print(e)
+        pass
+
+    finally:
+        pool.close()
